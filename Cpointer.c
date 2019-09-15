1631c52927cf33629d8a16359a435fcad5bf478f
Amit Singh <cm8204@hotmail.com>
2019-09-15 13:40:55 +0530

    First commit

--- Cpointer.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int x = 10;
-    int *ptr = &x;
-    *ptr = 20;
-    printf("%d\n", *ptr);
-    return 0;
-}
---
+++ Stack.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+public class Stack{
+	int lTop;
+	int[] arr;
+	int lCapacity;
+	public Stack(int aSize){
+		lCapacity = aSize;
+		lTop = -1;
+		arr = new int[aSize];
+	}
+
+	public void push(int aValue){
+		if(isFull()){
+			System.out.println("Stack is overflow");
+			System.exit(1);
+		}
+		System.out.println("inserting... "+aValue);
+		arr[++lTop] = aValue;
+	}
+
+	public int pop(){
+		if(isEmpty()){
+			System.out.println("Stack is underFlow");
+			System.exit(1);
+		}else{
+			System.out.println("Removing data... "+arr[lTop]);
+		}
+		return arr[lTop--];
+	}
+
+	public boolean isEmpty(){
+		return lTop == -1;
+	}
+
+	public boolean isFull(){
+		return lTop == lCapacity - 1;
+	}
+
+	public static void main(String[] args){
+		Stack lObj = new Stack(4);
+
+		System.out.println("Please Enter a Value");
+		Scanner lSc1 = new Scanner(System.in);
+		int lValue1 = lSc1.nextInt();
+		Scanner lSc2 = new Scanner(System.in);
+		int lValue2 = lSc2.nextInt();
+		Scanner lSc3 = new Scanner(System.in);
+		int lValue3 = lSc3.nextInt();
+		Scanner lSc4 = new Scanner(System.in);
+		int lValue4 = lSc4.nextInt();
+
+		lObj.push(lValue1);
+		lObj.push(lValue2);
+		lObj.push(lValue3);
+		lObj.push(lValue4);
+		lObj.pop();
+		lObj.pop();
+		lObj.pop();
+		lObj.pop();
+		lObj.pop();
+
+
+	}
+}
